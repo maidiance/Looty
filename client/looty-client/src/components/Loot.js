029182aed2792d6f266e0cb089d8ef14497ee851
Winifred Lu <winnie.lu@comcast.net>
2021-12-19 17:24:26 -0500

    adding buttons, functionality to come later

--- client/looty-client/src/components/Loot.js
+++ client/looty-client/src/components/Loot.js
@@ -5,25 +5,36 @@ const StyledDiv = styled.div`
     display: flex;
     width: 25%;
     flex-wrap: wrap;
+    justify-content: center;
     margin: 0 auto;
-    padding: 1% 3%;
+    padding: 1% 2%;
     background-color: ${props => props.theme.darkerBrown};
     color: ${props => props.theme.lighterBrown};
     border-style: dashed;
     border-color: ${props => props.theme.lightBrown};
+    #sellBtn {
+        width: 90%;
+        padding: 1%;
+        margin: 1% auto;
+    }
 `
 
-function Loot({ details }) {
+function Loot({ details, editLoot, deleteLoot, sellLoot }) {
     if (!details) {
         return <h3>Working fetching your loot...</h3>
     }
     return (
         <StyledDiv className='loot container'>
+            <div className='topButtons'>
+                <button onclick={editLoot(details.id)} id='editBtn'>✏️</button>
+                <button onclick={deleteLoot(details.id)} id='deleteBtn'>❌</button>
+            </div>
             <div className='lootDetails'>
                 <p>Name: {details.name}</p>
                 <p>Value: {details.value}</p>
                 <p>Claimed by: {details.claimedBy || 'none'}</p>
             </div>
+            <button onclick={sellLoot(details.id)} id='sellBtn'>Sell Loot</button>
         </StyledDiv>
     )
 }
--- client/looty-client/src/components/LootDisplay.js
+++ client/looty-client/src/components/LootDisplay.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Loot from './Loot';
 
 const StyledDiv = styled.div`
+    box-sizing: border-box;
     display: flex;
     width: 100%;
     flex-wrap: wrap;
@@ -12,12 +13,33 @@ const StyledDiv = styled.div`
 `
 
 export default function LootDisplay(props){
+    const editLoot = (lootId) => {
+        // allow user to make changes
+        // push changes to db
+    }
+    const deleteLoot = (lootId) => {
+        // delete loot from lootBag
+        // push changes to db
+    }
+    const sellLoot = (lootId) => {
+        // we will probably need playerId too
+        // remove loot from lootBag and put into soldBag ?
+        // claimedBy = 'sold'
+        // push changes to db
+    }
+
     return (
         <StyledDiv className='lootDisplay'>
             {
                 props.lootBag.map(loot => {
                 return(
-                    <Loot key={loot.id} details={loot} />
+                    <Loot 
+                        key={loot.id}
+                        details={loot}
+                        editLoot={editLoot}
+                        deleteLoot={deleteLoot}
+                        sellLoot={sellLoot}
+                    />
                 )
                 })
             }
