25bbfa5d85453e4d48eba9a9c8b238db3ceae96b
Winifred Lu <winnie.lu@comcast.net>
2021-12-19 15:09:45 -0500

    basic header in place

---
+++ client/looty-client/src/theme/index.js
@@ -0,0 +1,9 @@
+export default {
+    lighterBrown: '#f3e9e0',
+    lightBrown: '#dcbca2',
+    brown: '#c48f65',
+    darkBrown: '#966339',
+    darkerBrown: '#6b4729',
+    black: '#313639',
+    white: '#f8f8ff',
+}
