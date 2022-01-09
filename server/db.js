97ecf8f9b0d881977ecfe0a562893cb56a08af01
Bo <23106933+bo-crandall@users.noreply.github.com>
2022-01-09 12:05:40 -0500

    intital server commit

--- server/db.js
+++ server/db.js
@@ -1,13 +1,29 @@
 const mysql = require('mysql')
-const db = mysql.createConnection({
-host: "10.164.3.103",
-user: "express",
-password: "express",
-database:"express" 
-})
+const prod = true;
+
+let db
+if (prod)
+{
+	db = mysql.createConnection({
+	host: "localhost",
+	user: "looty",
+	password: "looty-prod",
+	database:"looty" 
+	})
+}
+else 
+{
+	db = mysql.createConnection({
+	host: "localhost",
+	user: "looty-dev",
+	password: "looty-dev",
+	database:"looty-dev" 
+	})
+}
+
 
 db.connect()
 
-// console.log(db)
+console.log(db)
 
 module.exports = db;
