56f0032b1f0302fdf8e6ea58f8b4d4b0f44b67cc
Bo <23106933+bo-crandall@users.noreply.github.com>
2022-01-09 16:36:21 -0500

    incremental change

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
-var db         = require('./db.js')
+//var db         = require('./db.js')
 
 
 // configure app to use bodyParser()
@@ -21,7 +21,9 @@ var router = express.Router();              // get an instance of the express Ro
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
+    console.log("sending reply")
     res.json({ message: 'hooray! welcome to our api!' });
+    console.log("Hello there")
 });
 
 // more routes for our API will happen here
