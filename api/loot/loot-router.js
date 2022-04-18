48ffcd8bac27b97d5730150b5aa5a24ad62d1864
Winifred Lu <winnie.lu@comcast.net>
2022-04-18 13:31:35 -0400

    undistributed query params in get endpoint need tests

--- api/loot/loot-router.js
+++ api/loot/loot-router.js
@@ -5,13 +5,23 @@ const { restricted, only} = require('../middleware/restricted');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    Loots.get()
-        .then(loot => {
-            res.status(200).json(loot);
-        })
-        .catch(() => {
-            res.status(500).json({message: 'could not get all loot'});
-        })
+    if(req.query.undistributed){
+        Loots.getByFilter({claimed: false, bagged: false, sold: false})
+            .then(loot => {
+                res.status(200).json(loot);
+            })
+            .catch(() => {
+                res.status(500).json({message: 'could not get undistributed loot'});
+            })
+    } else {
+        Loots.get()
+            .then(loot => {
+                res.status(200).json(loot);
+            })
+            .catch(() => {
+                res.status(500).json({message: 'could not get all loot'});
+            })
+    }
 });
 
 router.get('/:id', validateLootId, (req, res) => {
